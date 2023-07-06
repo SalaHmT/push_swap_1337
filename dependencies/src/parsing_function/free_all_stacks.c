fb0423e0bbade743ac7d6b2671eda5bacc9d602c
Salah Hamsate <shamsate@e2r8p15.1337.ma>
2023-07-06 17:51:44 +0100

    parsing adding  2 file.c  > ...

---
+++ dependencies/src/parsing_function/free_all_stacks.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_all_stacks.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shamsate <shamsate@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/06 17:44:17 by shamsate          #+#    #+#             */
+/*   Updated: 2023/07/06 17:50:55 by shamsate         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../include/push_swap.h"
+
+void	free_stack(t_list *stk)
+{
+	t_list	*data;
+
+	while (stk)
+	{
+		data = stk;
+		stk = stk->next;
+		free(data);
+	}
+}
+
+void	free_all_stack(t_list **a, t_list **b)
+{
+	free_stack(*a);
+	free_stack(*b);
+}
+
+void	free_data(char **str)
+{
+	int	i;
+
+	i = -1;
+	while (*str[++i])
+	{
+		free (*str[i]);
+	}
+}
