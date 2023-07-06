c2636661bdb934223efe35a41ae773d33ba405af
Salah Hamsate <shamsate@e2r8p15.1337.ma>
2023-07-06 17:54:27 +0100

    parsing adding  function for free stacks  > ...

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: shamsate <shamsate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 18:45:13 by shamsate          #+#    #+#             */
-/*   Updated: 2023/07/06 17:31:03 by shamsate         ###   ########.fr       */
+/*   Updated: 2023/07/06 17:53:18 by shamsate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,20 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(const char *str, char c);
 char	*ft_substr(char	const *s, unsigned int start, size_t len);
 //*****************Struct libft *********
-void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstnew(void	*content);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 //********* Parsing *********************
 int		arg_len(char **str);
 int		is_all_digit(char *str);
 char	*join_arg(char **str, int x);
 char	**split_arg(char **str, int x);
 void	check_if_empty(char **str, int len);
+void	free_stack(t_list *stk);
+void	free_all_stack(t_list **a, t_list **b);
+void	free_data(char **str);
+
 // ******** Error Handling Msg **********
 void	check_nbr_error(void);
 
