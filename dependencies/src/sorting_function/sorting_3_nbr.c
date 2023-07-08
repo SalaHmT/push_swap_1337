b098ac3e49d5f2e53ceb6c15cef5726543b51ddd
Salah Hamsate <shamsate@e2r8p15.1337.ma>
2023-07-08 14:07:16 +0100

    Adding sorting function for 3 nbr .. :)

--- dependencies/src/sorting_function/sorting_3_nbr.c
+++ dependencies/src/sorting_function/sorting_3_nbr.c
@@ -6,7 +6,35 @@
 /*   By: shamsate <shamsate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 13:56:03 by shamsate          #+#    #+#             */
-/*   Updated: 2023/07/08 13:56:05 by shamsate         ###   ########.fr       */
+/*   Updated: 2023/07/08 14:06:28 by shamsate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../../include/push_swap.h"
+
+void	sort_3_nbr(t_list	**src)
+{
+	int	tmp;
+	int	tmpo;
+	int	tmpor;
+
+	tmp = (*src)->content;
+	tmpo = (*src)->next->content;
+	tmpor = (*src)->next->next->content;
+	if (tmp > tmpo && tmp < tmpor)
+		sa(src);
+	else if (tmp > tmpo && tmpo > tmpor)
+	{
+		sa(src);
+		rra(src);
+	}
+	else if (tmp < tmpo && tmp > tmpor)
+		rra(src);
+	else if (tmp > tmpor && tmp > tmpo)
+		ra(src);
+	else if (tmp < tmpor && tmpor < tmpo)
+	{
+		sa(src);
+		ra(src);
+	}
+}
